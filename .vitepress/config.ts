7deff0040058ff831cf661e3e05ebbd20eb8299f
tlagoda <thibautlagoda@hotmail.fr>
2023-01-26 22:06:48 +0100

    style-guide/rules-essential.md translation

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -537,7 +537,7 @@ export const sidebar: ThemeConfig['sidebar'] = {
           link: '/style-guide/'
         },
         {
-          text: 'A - Essential',
+          text: 'A - Essentiel',
           link: '/style-guide/rules-essential'
         },
         {
