9276d7a360412c9ad24027167244ddde0c69d882
tlagoda <thibautlagoda@hotmail.fr>
2023-02-05 14:28:07 +0100

    translation of title + references

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -354,7 +354,7 @@ export const sidebar: ThemeConfig['sidebar'] = {
       items: [
         { text: 'Application', link: '/api/application' },
         {
-          text: 'General',
+          text: 'Généralités',
           link: '/api/general'
         }
       ]
