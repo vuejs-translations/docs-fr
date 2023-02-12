23a72e84ac869ba0ad17449055c78e4bd82a3ba6
tlagoda <thibautlagoda@hotmail.fr>
2023-02-12 10:03:50 +0100

    translate title

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -425,7 +425,7 @@ export const sidebar: ThemeConfig['sidebar'] = {
       items: [
         { text: 'Syntax Specification', link: '/api/sfc-spec' },
         { text: '<script setup>', link: '/api/sfc-script-setup' },
-        { text: 'CSS Features', link: '/api/sfc-css-features' }
+        { text: 'Fonctionnalités CSS', link: '/api/sfc-css-features' }
       ]
     },
     {
