b2caf51b90c780877de49cb49f850a5f2d430c2f
Nathanaël <roipoussiere@protonmail.com>
2023-10-10 13:28:19 +0200

    grid example code: rename dict keys

--- src/examples/src/grid/App/composition.js
+++ src/examples/src/grid/App/composition.js
@@ -7,12 +7,12 @@ export default {
   },
   setup() {
     const searchQuery = ref('')
-    const gridColumns = ['name', 'power']
+    const gridColumns = ['nom', 'puissance']
     const gridData = [
-      { name: 'Chuck Norris', power: Infinity },
-      { name: 'Bruce Lee', power: 9000 },
-      { name: 'Jackie Chan', power: 7000 },
-      { name: 'Jet Li', power: 8000 }
+      { nom: 'Chuck Norris', puissance: Infinity },
+      { nom: 'Bruce Lee', puissance: 9000 },
+      { nom: 'Jackie Chan', puissance: 7000 },
+      { nom: 'Jet Li', puissance: 8000 }
     ]
 
     return {
--- src/examples/src/grid/App/options.js
+++ src/examples/src/grid/App/options.js
@@ -6,12 +6,12 @@ export default {
   },
   data: () => ({
     searchQuery: '',
-    gridColumns: ['name', 'power'],
+    gridColumns: ['nom', 'puissance'],
     gridData: [
-      { name: 'Chuck Norris', power: Infinity },
-      { name: 'Bruce Lee', power: 9000 },
-      { name: 'Jackie Chan', power: 7000 },
-      { name: 'Jet Li', power: 8000 }
+      { nom: 'Chuck Norris', puissance: Infinity },
+      { nom: 'Bruce Lee', puissance: 9000 },
+      { nom: 'Jackie Chan', puissance: 7000 },
+      { nom: 'Jet Li', puissance: 8000 }
     ]
   })
 }
