b4f16cad72c49d360ccf468b13c6dc2340ba5688
Michel EDIGHOFFER <edimitchel@gmail.com>
2024-08-02 08:18:34 +0200

    Update .vitepress/theme/components/SponsorsAside.vue
    Co-authored-by: Ismaël Maurice <55036198+tisma95@users.noreply.github.com>

--- .vitepress/theme/components/SponsorsAside.vue
+++ .vitepress/theme/components/SponsorsAside.vue
@@ -12,7 +12,7 @@ const { frontmatter } = useData()
   >
     <img width="22" height="22" src="/viteconf.svg" />
     <span>
-      <p class="extra-info">Building Together</p>
+      <p class="extra-info">Construire ensemble</p>
       <p class="heading">ViteConf 24 - Oct 3</p>
       <p class="extra-info">Get your free ticket!</p>
     </span>
