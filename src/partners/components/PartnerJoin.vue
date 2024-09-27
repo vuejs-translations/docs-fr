71748d94ccc4fbab9bcf21d7f01dd854f268cf7b
Ismaël Maurice <ismael.tuo@edigrp.com>
2024-09-27 19:21:52 +0200

    Chore(PartnerJoin): Integrate the CallToActionSection component #1367
    #1367

--- src/partners/components/PartnerJoin.vue
+++ src/partners/components/PartnerJoin.vue
@@ -1,70 +1,12 @@
+<script setup lang="ts">
+import CallToActionSection from '@theme/components/CallToActionSection.vue'
+</script>
+
 <template>
-  <div class="divider"></div>
-  <div class="join-container">
-    <h2>Développez votre business avec Vue</h2>
-    <p>
-      Générez des prospects pour vos nouveaux projets, boostez votre crédibilité aux yeux
-      de vos clients, et soutenez la durabilité à long terme de Vue.js et de son
-      écosystème.
-    </p>
-    <a
-      class="join"
-      href="https://airtable.com/shrCQhat57SApJI2l"
-      target="_blank"
-      >Devenez partenaire</a
-    >
-  </div>
+ <CallToActionSection
+    title="Développez votre activité avec Vue"
+    description="Générez des pistes pour de nouveaux projets, renforcez votre crédibilité auprès de vos clients et soutenez la viabilité à long terme de Vue.js et de son écosystème."
+    link="https://airtable.com/shrCQhat57SApJI2l"
+    linkText="Devenir partenaire"
+  />
 </template>
-
-<style scoped>
-.divider {
-  width: 100px;
-  margin: 0px auto;
-  border-top: 1px solid var(--vt-c-divider-light);
-}
-
-.join-container {
-  max-width: 688px;
-  padding: 28px 28px 96px;
-  text-align: center;
-  margin: 0 auto;
-}
-
-.lead {
-  color: var(--vt-c-text-2);
-  font-weight: 500;
-  font-size: 18px;
-}
-
-h2 {
-  font-weight: 600;
-  letter-spacing: -0.5px;
-  font-size: 34px;
-  line-height: 1.2;
-  margin: 0.5em 0 1em;
-}
-
-p {
-  color: var(--vt-c-text-2);
-}
-
-.join {
-  margin-top: 2em;
-  display: inline-block;
-  text-align: center;
-  background-color: var(--vt-c-bg-mute);
-  color: var(--vt-c-text-code);
-  padding: 12px 24px;
-  font-weight: 600;
-  border-radius: 6px;
-  transition: background-color 0.5s, color 0.5s;
-}
-
-.join:hover {
-  background-color: var(--vt-c-gray-light-4);
-}
-
-.dark .join:hover {
-  background-color: var(--vt-c-gray-dark-3);
-}
-</style>
