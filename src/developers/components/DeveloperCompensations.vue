1fd1ca8656161d7d16a41bfc9606093112ea7519
Ismaël Maurice <55036198+tisma95@users.noreply.github.com>
2024-09-27 22:26:35 +0200

    1367 add developers pages proxify as a partner (#1377)
    Co-authored-by: Michel EDIGHOFFER <edimitchel@gmail.com>

---
+++ src/developers/components/DeveloperCompensations.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { DeveloperCompensations } from './type'
+
+defineProps<{
+  compensations?: DeveloperCompensations
+  showDetails?: boolean
+  title?: string
+}>()
+</script>
+
+<template>
+  <div>
+    <h4 v-if="title">{{ title }}</h4>
+    <div>
+      <div v-if="compensations" class="developer-compensations__list">
+        <div v-for="(compensation, key) in compensations" :key="`compensation-${key}`">
+          {{ compensation }}
+        </div>
+      </div>
+      <div v-if="showDetails" class="developer-compensations__details">
+        <p>Pas d'autres coûts.</p>
+        <p>Période d'essai d'un mois, puis préavis d'un mois.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.developer-compensations__list {
+  display: flex;
+  flex-direction: column;
+}
+
+.developer-compensations__details {
+  margin-top: 32px;
+}
+</style>
---
+++ src/developers/index.md
@@ -0,0 +1,11 @@
+---
+page: true
+footer: false
+title: Développeurs de Vue.js
+---
+
+<script setup>
+import DeveloperLanding from './components/DeveloperLanding.vue'
+</script>
+
+<DeveloperLanding />
