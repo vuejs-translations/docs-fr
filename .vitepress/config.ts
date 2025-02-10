7a98349b7503ff39522559d3cdc472ba868153db
Ismaël Maurice <55036198+tisma95@users.noreply.github.com>
2025-02-10 23:49:34 +0100

    Feat(theme): Update sponsors page #1562 (#1563)

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -666,7 +666,7 @@ export default defineConfigWithTheme<ThemeConfig>({
       'link',
       {
         rel: 'preconnect',
-        href: 'https://sponsors.vuejs.org'
+        href: 'https://automation.vuejs.org'
       }
     ],
     [
