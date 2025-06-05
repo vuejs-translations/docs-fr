29fcf4e5bd54083fc948b94c19c4fce1131cd42a
Ismaël Maurice <ismael.tuo@edigrp.com>
2025-06-05 13:47:58 +0200

    Feat: Register ScrimbaLink in app

--- .vitepress/theme/components/ScrimbaLink.vue
+++ .vitepress/theme/components/ScrimbaLink.vue
@@ -39,7 +39,7 @@ export default {
     top: calc(50% - 15px);
     left: -4px;
     border-radius: 50%;
-    background-color: #2B2B2B;
+    background-color: #73abfe;
     /* Scrimba's dark color */
 }
 
